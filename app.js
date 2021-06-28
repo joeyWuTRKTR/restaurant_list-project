21a1d4b135248b9acf8f15f87db923c38a1389fa
WilliamTsou818 <cccc1589@gmail.com>
2021-06-28 13:29:44 +0800

    modify: render index page by seeder

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const Restaurant = require('./models/restaurant')
 
 const app = express()
 const PORT = 3000
@@ -7,11 +8,23 @@ const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// include mongoose and connect to database
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => { console.error('Failed to connect to mongoDB!') })
+db.once('open', () => { console.log('The server is connected to mongoDB!') })
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.error(error))
 })
 
 app.listen(PORT, () => {
