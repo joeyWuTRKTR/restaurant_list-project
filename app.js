99566cde3aba0f6f71fd3169b9da7e01c12d18ba
WilliamTsou818 <cccc1589@gmail.com>
2021-06-28 13:11:23 +0800

    feat: add default layout and index page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
