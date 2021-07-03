983c06d38bc97698d846322157258798c0782bf4
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-03 08:22:01 +0800

    initial project

---
+++ app.js
@@ -0,0 +1,44 @@
+// import express
+const express = require('express')
+const app = express()
+
+// default port
+const port = 3000
+
+// import restaurant list 
+const restaurantList = require('./restaurant.json')
+
+// import handlebars template engine
+const exphbs = require('express-handlebars')
+
+// set template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// set static files
+app.use(express.static('public'))
+
+// define router
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// define show router
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  console.log(restaurant)
+  res.render('show', { restaurant: restaurant })
+})
+
+// search router
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+
+// set listener
+app.listen(port, () => {
+  console.log('Updated!')
+})
