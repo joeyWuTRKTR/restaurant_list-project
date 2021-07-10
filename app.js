a159a26b3ccb1dd96d506f1ac8628a7f597b9b8a
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-10 22:38:56 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -156,6 +156,13 @@ app.post('/restaurants/:id/edit', (req, res) => {
 })
 
 // Delete
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 // search router
 app.get('/search', (req, res) => {
